6eded853005951641c7b2c3305c07b0a92aa87e0
Cédric Luthi <cedric.luthi@gmail.com>
2022-06-15 10:54:04 +0200

    Prefer DIRECT connection if available

--- pac/selector.go
+++ pac/selector.go
@@ -4,6 +4,11 @@ package pac
 type FirstItemSelector struct{}
 
 func (s *FirstItemSelector) SelectProxy(from Proxies) Proxy {
+	for _, proxy := range from {
+		if proxy == DirectProxy {
+			return DirectProxy
+		}
+	}
 	if len(from) < 1 {
 		return DirectProxy
 	}
